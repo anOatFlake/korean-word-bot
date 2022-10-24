a38c29ac258d8837c77f47696bfcc11260ec1f8d
anOatFlake <annika@flock.cc>
2022-10-24 21:38:45 +0200

    update for transate and wordOfTheDay command

--- src/commands/translate.ts
+++ src/commands/translate.ts
@@ -1,12 +1,18 @@
 // https://nodejsera.com/how-to-use-google-translator-with-nodejs.html
 
-import { SlashCommandBuilder, Message} from 'discord.js';
+import { Client, CommandInteraction} from 'discord.js';
+import { Command } from 'src/models/command';
 
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('translate')
-        .setDescription('Übersetze ein koreanischen Wort nach Deutsch'),
-	async excecute(interaction: Message) { //TODO: add translation function here
-        await interaction.reply("Insert Translation here");
+export const translate: Command = {
+    name: "translate",
+    description: "translates the following korean word",
+    //type: "CHAT_INPUT",
+    run: async (client: Client, interaction: CommandInteraction) => {
+        const content = "INSERT KOREAN TRANSLATION WORD HERE";
+
+        await interaction.followUp({
+            ephemeral: true,
+            content
+        });
     }
-}
+}; 
--- src/commands/word-of-the-day.ts
+++ src/commands/word-of-the-day.ts
@@ -1,15 +1,23 @@
 // https://github.com/kelektiv/node-cron#readme
 
-import { Interaction, Message, SlashCommandBuilder } from 'discord.js';
+import { Client, CommandInteraction } from 'discord.js';
+import { Command } from "../models/command";
+
 
 const messageString = "Word of the day: "
 const wordPair = ""
 
-module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('word')
-		.setDescription(''),
-	async execute(message: Message) {
-		return message.reply("TESTETSTWT");
-	},
-};
+
+export const wordOfTheDay: Command = {
+    name: "word-of-the-day",
+    description: "Returns the korean word of the day",
+    //type: "CHAT_INPUT",
+    run: async (client: Client, interaction: CommandInteraction) => {
+        const content = "Hello there!";
+
+        await interaction.followUp({
+            ephemeral: true,
+            content
+        });
+    }
+}; 

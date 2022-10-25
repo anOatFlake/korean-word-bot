d73daadf9fe32e140e24f9c2a22c7467aaffda5f
anOatFlake <annika@flock.cc>
2022-10-25 06:09:46 +0200

    prettier reformat

--- src/commands/translate.ts
+++ src/commands/translate.ts
@@ -1,18 +1,18 @@
 // https://nodejsera.com/how-to-use-google-translator-with-nodejs.html
 
-import { Client, CommandInteraction} from 'discord.js';
-import { Command } from 'src/models/command';
+import { Client, CommandInteraction } from 'discord.js'
+import { Command } from 'src/models/command'
 
 export const translate: Command = {
-    name: "translate",
-    description: "translates the following korean word",
+    name: 'translate',
+    description: 'translates the following korean word',
     //type: "CHAT_INPUT",
     run: async (client: Client, interaction: CommandInteraction) => {
-        const content = "INSERT KOREAN TRANSLATION WORD HERE";
+        const content = 'INSERT KOREAN TRANSLATION WORD HERE'
 
         await interaction.followUp({
             ephemeral: true,
-            content
-        });
-    }
-}; 
+            content,
+        })
+    },
+}
--- src/commands/word-of-the-day.ts
+++ src/commands/word-of-the-day.ts
@@ -1,23 +1,21 @@
 // https://github.com/kelektiv/node-cron#readme
 
-import { Client, CommandInteraction } from 'discord.js';
-import { Command } from "../models/command";
+import { Client, CommandInteraction } from 'discord.js'
+import { Command } from '../models/command'
 
-
-const messageString = "Word of the day: "
-const wordPair = ""
-
+const messageString = 'Word of the day: '
+const wordPair = ''
 
 export const wordOfTheDay: Command = {
-    name: "word-of-the-day",
-    description: "Returns the korean word of the day",
+    name: 'word-of-the-day',
+    description: 'Returns the korean word of the day',
     //type: "CHAT_INPUT",
     run: async (client: Client, interaction: CommandInteraction) => {
-        const content = "Hello there!";
+        const content = 'Hello there!'
 
         await interaction.followUp({
             ephemeral: true,
-            content
-        });
-    }
-}; 
+            content,
+        })
+    },
+}
